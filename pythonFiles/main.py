c78fccd30e9a530d2a35aa5b1f968bd237526bf9
Harry Marshall <harryyymarshall@gmail.com>
2023-09-07 12:56:54 +1000

    changed image names and loadedpygame images

--- pythonFiles/main.py
+++ pythonFiles/main.py
@@ -2,6 +2,7 @@ import pygame as p
 from chessBoard import ChessBoard
 
 gameBoard = ChessBoard()
+print(gameBoard.board)
 
 WIDTH, HEIGHT = 512, 512
 DIMENSION = 8
@@ -10,8 +11,9 @@ MAX_FPS = 15
 IMAGES = {}
 
 def loadImages(piece):
-    pieces = ["wp", ]
-    IMAGES[piece] = p.image.load("python/chessAI/images" + piece + ".png")
+    pieces = ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR", "bP", "wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR", "wP"]
+    for piece in range(len(pieces)):
+        IMAGES[piece] = p.image.load("python/chessAI/images" + piece + ".png")
 
 p.display.set_mode((WIDTH, HEIGHT))
 p.display.set_caption("Drag and Drop Chess")
