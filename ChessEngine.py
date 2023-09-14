e5a43955b8e4dae9ca5779969caea6ec0b657169
Harry Marshall <harryyymarshall@gmail.com>
2023-09-14 14:58:57 +1000

    begin rook move

--- ChessEngine.py
+++ ChessEngine.py
@@ -35,52 +35,52 @@ class GameState():
     def getAllPossibleMoves(self):
         moves = []
 
-        for row in range(len(self.board)):
-            for column in range(len(self.board[row])):
-                turn = self.board[row][column][0]
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                turn = self.board[r][c][0]
                 if (turn == "w" and self.whiteToMove) or (turn == "b" and not self.whiteToMove):
-                    piece = self.board[row][column][1]
-                    self.moveFunctions[piece](row, column, moves)
+                    piece = self.board[r][c][1]
+                    self.moveFunctions[piece](r, c, moves)
         return moves
 
-    def getPawnMoves(self, row, column, moves):
+    def getPawnMoves(self, r, c, moves):
         if self.whiteToMove:
-            if self.board[row - 1][column] == "--":
-                moves.append(Move((row, column), (row - 1, column), self.board))
-                if row == 6 and self.board[row - 2][column] == "--":
-                    moves.append(Move((row, column), (row - 2, column), self.board))
-            if column - 1 >= 0:
-                if self.board[row - 1][column - 1][0] == "b":
-                    moves.append(Move((row, column), (row - 1, column - 1), self.board))
-            if column + 1 <= 7:
-                if self.board[row - 1][column + 1][0] == "b":
-                    moves.append(Move((row, column), (row - 1, column + 1), self.board))
+            if self.board[r - 1][c] == "--":
+                moves.append(Move((r, c), (r - 1, c), self.board))
+                if r == 6 and self.board[r - 2][c] == "--":
+                    moves.append(Move((r, c), (r - 2, c), self.board))
+            if c - 1 >= 0:
+                if self.board[r - 1][c - 1][0] == "b":
+                    moves.append(Move((r, c), (r - 1, c - 1), self.board))
+            if c + 1 <= len(self.board) - 1:
+                if self.board[r - 1][c + 1][0] == "b":
+                    moves.append(Move((r, c), (r - 1, c + 1), self.board))
 
         else:
-            if self.board[row + 1][column] == "--":
-                moves.append(Move((row, column), (row + 1, column), self.board))
-                if row == 1 and self.board[row + 2][column] == "--":
-                    moves.append(Move((row, column), (row + 2, column), self.board))
-            if column - 1 >= 0:
-                if self.board[row - 1][column - 1][0] == "b":
-                    moves.append(Move((row, column), (row + 1, column - 1), self.board))
-            if column + 1 <= 7:
-                if self.board[row - 1][column + 1][0] == "b":
-                    moves.append(Move((row, column), (row + 1, column + 1), self.board))
+            if self.board[r + 1][c] == "--":
+                moves.append(Move((r, c), (r + 1, c), self.board))
+                if r == 1 and self.board[r + 2][c] == "--":
+                    moves.append(Move((r, c), (r + 2, c), self.board))
+            if c - 1 >= 0:
+                if self.board[r - 1][c - 1][0] == "b":
+                    moves.append(Move((r, c), (r + 1, c - 1), self.board))
+            if c + 1 <= len(self.board) - 1:
+                if self.board[r - 1][c + 1][0] == "b":
+                    moves.append(Move((r, c), (r + 1, c + 1), self.board))
 
-    def getRookMoves(self, row, column, moves):
+    def getRookMoves(self, r, c, moves):
         pass
 
-    def getKnightMoves(self, row, column, moves):
+    def getKnightMoves(self, r, c, moves):
         pass
 
-    def getBishopMoves(self, row, column, moves):
+    def getBishopMoves(self, r, c, moves):
         pass
 
-    def getQueenMoves(self, row, column, moves):
+    def getQueenMoves(self, r, c, moves):
         pass
 
-    def getKingMoves(self, row, column, moves):
+    def getKingMoves(self, r, c, moves):
         pass
 
 
@@ -88,10 +88,10 @@ class Move():
 
     ranksToRows = {"1" : 7, "2" : 6, "3" : 5, "4" : 4,
                    "5" : 3, "6" : 2, "7" : 1, "8" : 0}
-    rowsToRanks = {v : k for k, v in ranksToRows.items()}
+    rsToRanks = {v : k for k, v in ranksToRows.items()}
     filesToColumns = {"a" : 0, "b" : 1, "c" : 2, "d" : 3, 
                       "e" : 4, "f" : 5, "g" : 6, "h" : 7}
-    columnsToFiles = {v : k for k, v in filesToColumns.items()}
+    csToFiles = {v : k for k, v in filesToColumns.items()}
 
     def __init__(self, startSquare, endSquare, board):
         self.startRow = int(startSquare[0])
@@ -112,4 +112,4 @@ class Move():
         return self.getRankFile(self.startRow, self.startColumn) + self.getRankFile(self.endRow, self.endColumn)
 
     def getRankFile(self, r, c):
-        return self.columnsToFiles[c] + self.rowsToRanks[r]
+        return self.csToFiles[c] + self.rsToRanks[r]
